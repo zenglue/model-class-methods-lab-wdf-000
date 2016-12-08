062ee0ba46fe4c06dd09a692cc0d84b8b8eda440
zenglue <keonilrath@gmail.com>
2016-12-07 22:53:41 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,23 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where("classifications.name = ?", "Catamaran").uniq
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where("classifications.name = ?", "Sailboat").uniq
+  end
+
+  def self.motorboaters
+    includes(boats: :classifications).where("classifications.name = ?", "Motorboat").uniq
+  end
+
+  def self.talented_seamen
+    sailors.where(id: motorboaters.ids)
+  end
+
+  def self.non_sailors
+    all.where.not(id: sailors.ids)
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,12 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    distinct("name")
+  end
+
+  def self.longest
+    eager_load(:boats).order("boats.length desc").limit(2).uniq
+  end
 end
